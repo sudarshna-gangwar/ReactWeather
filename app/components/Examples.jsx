e474bbcd19dff70ed87ba7b343559d361892e8e7
sudarshna-gangwar <sudarshna.gangwar@gmail.com>
2017-11-06 15:05:14 +0530

    example list

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,20 @@
 var React = require('react');
-
-// var Examples = React.createClass({
-//   render: function(){
-//     return(
-//       <h3>Examples Component</h3>
-//     );
-//   }
-// });
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Hyderabad'>Hyderabad</Link>
+        </li>
+        <li>
+          <Link to='/?location=Mumbai'>Mumbai</Link>
+        </li>
+      </ol>
+    </div>
   );
 };
 
